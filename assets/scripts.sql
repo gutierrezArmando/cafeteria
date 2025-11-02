fc8a59c00d96e2bfbf1f5c7d57d99101f9e3dc05
Armando <37322555+gutierrezArmando@users.noreply.github.com>
2025-11-02 10:47:14 -0800

    Update scripts.sql

--- assets/scripts.sql
+++ assets/scripts.sql
@@ -20,4 +20,16 @@ INSERT INTO productos_menu (nombre, descripcion, precio, categoria) VALUES
 ('Americano', 'Doble shot de espresso diluido en agua caliente.', 3.00, 'Cafe'),
 ('Tarta de Zanahoria', 'Bizcocho especiado con nueces y glaseado de queso crema.', 5.75, 'Pastel');
 
+INSERT INTO productos_menu (nombre, descripcion, precio, categoria, imagen_url) VALUES
+('Espresso Clásico', 'El corazón de todo, intenso y aromático.', 3.50, 'Cafe','assets/images/expressoclasico.png'),
+('Latte Cremoso', 'Perfecta armonía de leche vaporizada y espresso.', 4.99, 'Cafe','assets/images/lattecremoso.png'),
+('Pastel de Chocolate', 'El acompañamiento ideal para tu bebida.', 5.50, 'Pastel','assets/images/pastelchocolate.png'),
+('Cappuccino Clásico', 'Partes iguales de espresso, leche vaporizada y generosa espuma.', 4.50, 'Cafe','assets/images/cappuccino.png'),
+('Mocha con Crema Batida', 'Deliciosa combinación de espresso, chocolate y crema batida.', 5.99, 'Cafe','assets/images/mocha.png'),
+('Cheesecake de Frutos Rojos', 'Tarta de queso suave y cremosa con salsa de frutos rojos frescos.', 6.50, 'Pastel','assets/images/frutos.png'),
+('Americano', 'Doble shot de espresso diluido en agua caliente.', 3.00, 'Cafe',''),
+('Tarta de Zanahoria', 'Bizcocho especiado con nueces y glaseado de queso crema.', 5.75, 'Pastel','');
+
 select * from productos_menu;
+
+
