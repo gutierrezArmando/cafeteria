38913a97fed71f56c534b2bb4ce5cd42f6e081d8
Armando <37322555+gutierrezArmando@users.noreply.github.com>
2025-11-02 08:42:52 -0800

    script para la bd
    Se agregan los scripts para la base de datos

---
+++ assets/scripts.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS cafeteria;
+use cafeteria;
+CREATE TABLE productos_menu (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+    descripcion TEXT,
+    precio DECIMAL(5, 2) NOT NULL,
+    categoria ENUM('Cafe', 'Pastel', 'Bebida Fria', 'Otro') NOT NULL,
+    imagen_url VARCHAR(255),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO productos_menu (nombre, descripcion, precio, categoria) VALUES
+('Espresso Clásico', 'El corazón de todo, intenso y aromático.', 3.50, 'Cafe'),
+('Latte Cremoso', 'Perfecta armonía de leche vaporizada y espresso.', 4.99, 'Cafe'),
+('Pastel de Chocolate', 'El acompañamiento ideal para tu bebida.', 5.50, 'Pastel'),
+('Cappuccino Clásico', 'Partes iguales de espresso, leche vaporizada y generosa espuma.', 4.50, 'Cafe'),
+('Mocha con Crema Batida', 'Deliciosa combinación de espresso, chocolate y crema batida.', 5.99, 'Cafe'),
+('Cheesecake de Frutos Rojos', 'Tarta de queso suave y cremosa con salsa de frutos rojos frescos.', 6.50, 'Pastel'),
+('Americano', 'Doble shot de espresso diluido en agua caliente.', 3.00, 'Cafe'),
+('Tarta de Zanahoria', 'Bizcocho especiado con nueces y glaseado de queso crema.', 5.75, 'Pastel');
+
+select * from productos_menu;
